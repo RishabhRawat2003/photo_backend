93946e59338897e4416ff59b6822687c73ad3db9
RishabhRawat2003 <rawatrishabh953@gmail.com>
2025-02-02 19:55:14 +0530

    update multer files

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,14 +1,22 @@
-import multer from 'multer'
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Define the /tmp directory
+const tempDir = "/tmp";
+
+// Ensure the /tmp directory exists
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./tmp")
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, tempDir); // Use /tmp directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Add timestamp to avoid conflicts
+  },
+});
 
-export const upload = multer({
-    storage
-})
+export const upload = multer({ storage });
